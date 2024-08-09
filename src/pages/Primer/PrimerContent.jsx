1bd06d24ffabcfbcfbe103075609c56a60f17a15
snaramirez872 <seanramirez818@gmail.com>
2024-08-08 21:10:28 -0400

    primer part 2

---
+++ src/pages/Primer/PrimerContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PrimerContent ({ children }) {
+    return (
+        <div className="topic-desc">
+            {children}
+        </div>
+    );
+}
