74baeebadc3a1a94e1cd6ea0432407ddac200205
snaramirez872 <seanramirez818@gmail.com>
2024-10-01 11:34:21 -0400

    Added info into Primer

--- src/pages/Primer/Primer.jsx
+++ src/pages/Primer/Primer.jsx
@@ -59,7 +59,11 @@ export default function Primer() {
                             <div className="introduction">
                                 <h1 className="sub-headers">Introduction</h1>
                                 <div className='intro-content'>
-                                    <p>[CONTENT HERE]</p>
+                                    <p>In this section, we will be going over the following</p>
+                                    <ul>
+                                        <li>How to set up your computer for coding projects</li>
+                                        <li>What is Git and how to use it</li>
+                                    </ul>
                                 </div>
                             </div>
                         </PrimerContent>
@@ -69,7 +73,45 @@ export default function Primer() {
                             <div className="setUp">
                                 <h1 className="sub-headers">Setting Up</h1>
                                 <div className='setup-content'>
-                                    <p>[CONTENT HERE]</p>
+                                    <div className="setup-intro">
+                                        <p>A computer is a software developer's best friend. It is where a majority, if not all, of our work gets done. 
+                                            Setting up your machine to make sure it's ready to go for coding is dependent on the following:
+                                        </p>
+                                        <ul>
+                                            <li>What application you are building and what language you are building it with</li>
+                                            <li>What Operating System (and version) that you use</li>
+                                            <li>Personal preference</li>
+                                        </ul>
+                                    </div>
+                                    <div className="ide-editor">
+                                        <h2>IDE or Text Editor?</h2>
+                                        <div className="ide">
+                                            <h3>Integrated Development Environment (IDE)</h3>
+                                            <p>
+                                                An Integrated Development Environment (IDE) is a software application that provides developers with everything 
+                                                that they would need when coding: a source code editor, debugger, and build automation tools. Any extra feautres 
+                                                are offered on a case-by-case basis. Some examples of IDEs include, but are not limited to:
+                                            </p>
+                                            <ul>
+                                                <li>Microsoft Vsual Studio Community for C and C++</li>
+                                                <li>PyCharm for Python</li>
+                                                <li>Intellj for Java</li>
+                                            </ul>
+                                        </div>
+                                        <div className="editor">
+                                        <h3>Text Editor</h3>
+                                        <p>
+                                            A Text editor at its core, as implied by the name, is a software application that allows for the inout of text to files that 
+                                            can be edited and saved in any format accordingly. Some examples of text editors include, but are not limited to:
+                                        </p>
+                                        <ul>
+                                            <li>Vim for Linux</li>
+                                            <li>Notepad for WIndows</li>
+                                            <li>Notes for MacOS</li>
+                                            <li>Microsoft Visual Studio Code</li>
+                                        </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </PrimerContent>
