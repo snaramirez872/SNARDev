df88536f82d4e09ee83ed1d6aa30122ecfa7f919
snaramirez872 <seanramirez818@gmail.com>
2024-11-02 16:56:06 -0400

    added more info to primer

--- src/pages/Primer/Primer.jsx
+++ src/pages/Primer/Primer.jsx
@@ -101,7 +101,7 @@ export default function Primer() {
                                         <div className="editor">
                                         <h3>Text Editor</h3>
                                         <p>
-                                            A Text editor at its core, as implied by the name, is a software application that allows for the inout of text to files that 
+                                            A Text editor at its core, as implied by the name, is a software application that allows for the input of text to files that 
                                             can be edited and saved in any format accordingly. Some examples of text editors include, but are not limited to:
                                         </p>
                                         <ul>
@@ -111,6 +111,50 @@ export default function Primer() {
                                             <li>Microsoft Visual Studio Code</li>
                                         </ul>
                                         </div>
+                                        <div className="ide-editor-two">
+                                            <div className="ideTwo">
+                                                <p>
+                                                    An IDE may seem beneficial to you for the fact that everything you need as a developer is provided to you: a means to run your code, 
+                                                    a debugger, and various qualify-of-life features depending on the software. The con, however, is that you are locked into 
+                                                    one or two coding languages per IDE. This means that if you are a versatile developer that knows several languages, you will have 
+                                                    several IDEs installed on your computer. This may seem troublesome if you are looking to save as much storage as possible.
+                                                </p>
+                                            </div>
+                                            <div className="editorTwo">
+                                                <p>
+                                                    If that's the case, a text editor may be more useful to you. A text editor allows for developing projects in many different languages 
+                                                    all from within the same editor. If you go this route, there are still a few things to consider. A text editor won't come with all the 
+                                                    bells and whistles that come with an IDE has. One of the few exceptions is Microsoft Visual Studio Code. VS Code comes with a first party 
+                                                    debugger and a slew of extensions that you can download. When it comes to actually running your programs you would have to do so manually.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="reccomendation">
+                                            <div className="partOne">
+                                                <h3>What I Reccomend</h3>
+                                                <p>
+                                                    In my day-to-day I personally use Microsoft Visual Studio Code. It is available to download and use for free on Windows, MacOS, and Linux Systems. 
+                                                    VS Code can be installed following the link <a href='https://code.visualstudio.com/download' target='_blank' rel='noreferrer'>here</a>.
+                                                </p>
+                                            </div>
+                                            <div className='partTwo'>
+                                                <h3>Windows Subsystem for Linux</h3>
+                                                <p className='disclaimer'>
+                                                    This section is for Windows Users only. MacOS systems do not need an additional tool like WSL to run code. If you use a MacOS or Linux system, you 
+                                                    may skip this section and go to "Useful UNIX Commands" below.
+                                                </p>
+                                                <p className='info'>
+                                                    In the event that you are using a text editor and have no way of running your programs, one option you can use is <b>Windows Subsystem for Linux (WSL)</b>. 
+                                                    WSL is a feature of Windows 10 and 11 that gives developers and enthusiasts access to a fully-functional Linux terminal from within their Windows device for free. This 
+                                                    is very useful for developers on Windows especially. It allows them to use a Linux server natively to run all of their code instead of having to deal with the 
+                                                    overhead of switching systems completely or dual-booting. WSL can be installed by following the instructions listed <a href='https://learn.microsoft.com/en-us/windows/wsl/install' target='_blank' rel='noreferrer'>here</a>.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        {/* 
+                                        TODO
+                                        - Add part about UNIX
+                                        */}
                                     </div>
                                 </div>
                             </div>
